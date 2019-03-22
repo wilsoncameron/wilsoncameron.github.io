2907c1d627d3f59578d5133dc254c90acd778021
wilsoncameron <wilsoncameron.nyc@gmail.com>
2019-03-21 22:38:25 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
   $("#clientlist").on('click', function() {
-      $(this).find('.clients').addClass('show');
+      $(this).find('.clients').toggleClass('show');
   });
 });
 
